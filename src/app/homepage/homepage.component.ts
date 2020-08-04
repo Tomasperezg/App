05944358416ab3d253ee30a43590c2824b5465fa
TomasP <perezgantomas@gmail.com>
2020-08-04 00:02:46 -0400

    Home page main banners updated

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,20 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
+
 export class HomepageComponent implements OnInit {
    // This is where button class/link/url can be edited
   buttonId = "Learn More";
   shtButton = "shortButton";
   url = "/about"
-  constructor(private router: Router) { }
 
-  ngOnInit() {
-  }
+
+  constructor() { }
+
+  ngOnInit() {  }
 
 
 }
